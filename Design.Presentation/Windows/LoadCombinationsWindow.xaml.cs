787b493bdc6d267ea76fad202e6e87dfca820734
Abdelkawy <Engineer_m.abdelkawy@yahoo.com>
2018-06-10 13:40:51 +0200

    Load Cases Window

---
+++ Design.Presentation/Windows/LoadCombinationsWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Design.Presentation.Windows
+{
+    /// <summary>
+    /// Interaction logic for LoadCombinationsWindow.xaml
+    /// </summary>
+    public partial class LoadCombinationsWindow : Window
+    {
+        public LoadCombinationsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddLoadComboBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DltLoadComboBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void loadComboSubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OkBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
