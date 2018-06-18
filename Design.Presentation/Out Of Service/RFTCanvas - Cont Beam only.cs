4b7b9627465e59443e0ed3b20bcdba8569fa8e6c
Abdelkawy <Engineer_m.abdelkawy@yahoo.com>
2018-06-18 09:39:47 +0200

    updates in RFTCanvas

--- Design.Presentation/Out Of Service/RFTCanvas - Cont Beam only.cs
+++ Design.Presentation/Out Of Service/RFTCanvas - Cont Beam only.cs
@@ -1,7 +1,6 @@
 ﻿using Design.Core.Sap;
 using Design.Presentation.Geometry;
 using Design.Presentation.Model;
-using Desing.Core.Sap;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -140,120 +139,45 @@ namespace Design.Presentation.ViewModels
         public static void ConstructColLines(GCanvas gCanvas, GeometryEngine GeometryEngineRFT, double scale)
         {
             /*----------------------Bottom Start Lines----------------------*/
-            //Case Of Cantilever at start
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[0].SelectedRestraint != Restraints.NoRestraints)
-            {
-                ColLinesBotStart[0] = new GLine(gCanvas, StartPointBotArr[0], new Point(StartPointBotArr[0].X, StartPointBotArr[0].Y + 0.4 * scale));
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(ColLinesBotStart[0]);
-            }
-            else
-            {
-                GLine comLineBotStart = new GLine(gCanvas, StartPointBotArr[0], new Point(StartPointBotArr[0].X - 0.15 * scale, StartPointBotArr[0].Y));
-                GeometryEngineRFT.Shapes["RFT"].Add(comLineBotStart);
-            }
-            for (int i = 1; i < ColLinesBotStart.Length; i++)
+            for (int i = 0; i < ColLinesBotStart.Length; i++)
             {
                 ColLinesBotStart[i] = new GLine(gCanvas, StartPointBotArr[i], new Point(StartPointBotArr[i].X, StartPointBotArr[i].Y + 0.4 * scale));
                 //add to the CANVAS 
                 GeometryEngineRFT.Shapes["RFT"].Add(ColLinesBotStart[i]);
             }
             /*----------------------Bottom End Lines----------------------*/
-            for (int i = 0; i < ColLinesBotEnd.Length - 1; i++)
+            for (int i = 0; i < ColLinesBotEnd.Length; i++)
             {
                 ColLinesBotEnd[i] = new GLine(gCanvas, EndPointBotArr[i], new Point(EndPointBotArr[i].X, EndPointBotArr[i].Y + 0.4 * scale));
                 //add to the CANVAS 
                 GeometryEngineRFT.Shapes["RFT"].Add(ColLinesBotEnd[i]);
             }
-            //Case Of Cantilever at End
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[SpanVals.Length].SelectedRestraint != Restraints.NoRestraints)
-            {
-                ColLinesBotEnd[ColLinesBotEnd.Length - 1] = new GLine(gCanvas, EndPointBotArr[ColLinesBotEnd.Length - 1], new Point(EndPointBotArr[ColLinesBotEnd.Length - 1].X, EndPointBotArr[ColLinesBotEnd.Length - 1].Y + 0.4 * scale));
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(ColLinesBotEnd[ColLinesBotEnd.Length - 1]);
-            }
-            else
-            {
-                GLine comLineBotEnd = new GLine(gCanvas, EndPointBotArr[ColLinesBotEnd.Length - 1], new Point(EndPointBotArr[ColLinesBotEnd.Length - 1].X + 0.15 * scale, EndPointBotArr[ColLinesBotEnd.Length - 1].Y));
-                GeometryEngineRFT.Shapes["RFT"].Add(comLineBotEnd);
-            }
 
             /*-----------------Top Start Lines---------------*/
-            //Case Of Cantilever at start
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[0].SelectedRestraint != Restraints.NoRestraints)
-            {
-                ColLinesTopStart[0] = new GLine(gCanvas, StartPointTopArr[0], new Point(StartPointTopArr[0].X, StartPointTopArr[0].Y - 0.4 * scale));
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(ColLinesTopStart[0]);
-            }
-            else
-            {
-                GLine comLineTopStart = new GLine(gCanvas, StartPointTopArr[0], new Point(StartPointTopArr[0].X - 0.15 * scale, StartPointTopArr[0].Y));
-                GeometryEngineRFT.Shapes["RFT"].Add(comLineTopStart);
-            }
-
-            for (int i = 1; i < ColLinesTopStart.Length; i++)
+            for (int i = 0; i < ColLinesTopStart.Length; i++)
             {
                 ColLinesTopStart[i] = new GLine(gCanvas, StartPointTopArr[i], new Point(StartPointTopArr[i].X, StartPointTopArr[i].Y - 0.40 * scale));
                 //add to the CANVAS 
                 GeometryEngineRFT.Shapes["RFT"].Add(ColLinesTopStart[i]);
             }
-
-            /*-----------------Top End Lines---------------*/
-            for (int i = 0; i < ColLinesTopEnd.Length - 1; i++)
+            //Top End Lines
+            for (int i = 0; i < ColLinesTopEnd.Length; i++)
             {
                 ColLinesTopEnd[i] = new GLine(gCanvas, EndPointTopArr[i], new Point(EndPointTopArr[i].X, EndPointTopArr[i].Y - 0.40 * scale));
                 //add to the CANVAS 
                 GeometryEngineRFT.Shapes["RFT"].Add(ColLinesTopEnd[i]);
             }
 
-            //Case Of Cantilever at End
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[SpanVals.Length].SelectedRestraint != Restraints.NoRestraints)
-            {
-                ColLinesTopEnd[ColLinesTopEnd.Length - 1] = new GLine(gCanvas, EndPointTopArr[ColLinesBotEnd.Length - 1], new Point(EndPointTopArr[ColLinesBotEnd.Length - 1].X, EndPointTopArr[ColLinesBotEnd.Length - 1].Y - 0.4 * scale));//---
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(ColLinesTopEnd[ColLinesTopEnd.Length - 1]);
-            }
-            else
-            {
-                GLine comLineTopEnd = new GLine(gCanvas, EndPointTopArr[ColLinesBotEnd.Length - 1], new Point(EndPointTopArr[ColLinesBotEnd.Length - 1].X + 0.15 * scale, EndPointTopArr[ColLinesBotEnd.Length - 1].Y));
-                GeometryEngineRFT.Shapes["RFT"].Add(comLineTopEnd);
-            }
-
             /*-------------Start and End Column Lines-------------*/
-            //Start Column Line
-            //Case of Cantilever at start:
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[0].SelectedRestraint != Restraints.NoRestraints)
-            {
-                GLine startColLine = new GLine(gCanvas, new Point(StartPointTopArr[0].X - 0.15 * scale, StartPointTopArr[0].Y - 0.40 * scale)
+            GLine startColLine = new GLine(gCanvas, new Point(StartPointTopArr[0].X - 0.15 * scale, StartPointTopArr[0].Y - 0.40 * scale)
                 , new Point(StartPointBotArr[0].X - 0.15 * scale, StartPointBotArr[0].Y + 0.40 * scale));
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(startColLine);
-            }
-            else
-            {
-                GLine startColLine = new GLine(gCanvas, new Point(StartPointTopArr[0].X - 0.15 * scale, StartPointTopArr[0].Y)
-                , new Point(StartPointBotArr[0].X - 0.15 * scale, StartPointBotArr[0].Y));
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(startColLine);
-            }
+            //add to the CANVAS 
+            GeometryEngineRFT.Shapes["RFT"].Add(startColLine);
 
-            //End Colimn Line
-            //Case of Cantilever at end
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[SpanVals.Length].SelectedRestraint != Restraints.NoRestraints)
-            {
-                GLine endColLine = new GLine(gCanvas, new Point(EndPointTopArr[SpanVals.Length - 1].X + 0.15 * scale, EndPointTopArr[SpanVals.Length - 1].Y - 0.40 * scale)
+            GLine endColLine = new GLine(gCanvas, new Point(EndPointTopArr[SpanVals.Length - 1].X + 0.15 * scale, EndPointTopArr[SpanVals.Length - 1].Y - 0.40 * scale)
                 , new Point(EndPointBotArr[SpanVals.Length - 1].X + 0.15 * scale, EndPointBotArr[SpanVals.Length - 1].Y + 0.40 * scale));
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(endColLine);
-            }
-            else
-            {
-                GLine endColLine = new GLine(gCanvas, new Point(EndPointTopArr[SpanVals.Length - 1].X + 0.15 * scale, EndPointTopArr[SpanVals.Length - 1].Y)
-                , new Point(EndPointBotArr[SpanVals.Length - 1].X + 0.15 * scale, EndPointBotArr[SpanVals.Length - 1].Y));
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(endColLine);
-            }
+            //add to the CANVAS 
+            GeometryEngineRFT.Shapes["RFT"].Add(endColLine);
         }
 
         #endregion
@@ -262,39 +186,20 @@ namespace Design.Presentation.ViewModels
         #region Longitudinal Reinforcement
         public static void BotRFT(GCanvas gCanvas, GeometryEngine GeometryEngineRFT, double scale)
         {
-            //Case of Cantilever at start
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[0].SelectedRestraint != Restraints.NoRestraints)
-            {
-                BotRFTLines[0] = new GLine(gCanvas, new Point(StartPointBotArr[0].X + 0.15 * (EndPointBotArr[0].X - StartPointBotArr[0].X), StartPointBotArr[0].Y - 0.07 * scale),
-                    new Point(StartPointBotArr[0].X + 0.85 * (EndPointBotArr[0].X - StartPointBotArr[0].X), StartPointBotArr[0].Y - 0.07 * scale));
-                GeometryEngineRFT.Shapes["RFT"].Add(BotRFTLines[0]);
-            }
-            for (int i = 1; i < BotRFTLines.Length - 1; i++)
+            for (int i = 0; i < BotRFTLines.Length; i++)
             {
                 BotRFTLines[i] = new GLine(gCanvas, new Point(StartPointBotArr[i].X + 0.15 * (EndPointBotArr[i].X - StartPointBotArr[i].X), StartPointBotArr[i].Y - 0.07 * scale),
                     new Point(StartPointBotArr[i].X + 0.85 * (EndPointBotArr[i].X - StartPointBotArr[i].X), StartPointBotArr[i].Y - 0.07 * scale));
                 //add to the CANVAS 
                 GeometryEngineRFT.Shapes["RFT"].Add(BotRFTLines[i]);
             }
-            //Case of Cantilever End
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[SpanVals.Length].SelectedRestraint != Restraints.NoRestraints)
-            {
-                BotRFTLines[BotRFTLines.Length - 1] = new GLine(gCanvas, new Point(StartPointBotArr[BotRFTLines.Length - 1].X + 0.15 * (EndPointBotArr[BotRFTLines.Length - 1].X - StartPointBotArr[BotRFTLines.Length - 1].X), StartPointBotArr[BotRFTLines.Length - 1].Y - 0.07 * scale),
-                    new Point(StartPointBotArr[BotRFTLines.Length - 1].X + 0.85 * (EndPointBotArr[BotRFTLines.Length - 1].X - StartPointBotArr[BotRFTLines.Length - 1].X), StartPointBotArr[BotRFTLines.Length - 1].Y - 0.07 * scale));
-                GeometryEngineRFT.Shapes["RFT"].Add(BotRFTLines[BotRFTLines.Length - 1]);
-            }
         }
         public static void TopRFT(GCanvas gCanvas, GeometryEngine GeometryEngineRFT, double scale)
         {
-            //Case Of Cantilever Start Span
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[0].SelectedRestraint != Restraints.NoRestraints)
-            {
-                TopRFTLines[0] = new GLine(gCanvas, new Point(StartPointTopArr[0].X, StartPointTopArr[0].Y + 0.07 * scale),
+            TopRFTLines[0] = new GLine(gCanvas, new Point(StartPointTopArr[0].X, StartPointTopArr[0].Y + 0.07 * scale),
                 new Point(StartPointTopArr[0].X + 0.20 * (EndPointTopArr[0].X - StartPointTopArr[0].X), StartPointTopArr[0].Y + 0.07 * scale));
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(TopRFTLines[0]);
-            }
-            
+            //add to the CANVAS 
+            GeometryEngineRFT.Shapes["RFT"].Add(TopRFTLines[0]);
 
             for (int i = 1; i < TopRFTLines.Length - 1; i++)
             {
@@ -304,15 +209,10 @@ namespace Design.Presentation.ViewModels
                 GeometryEngineRFT.Shapes["RFT"].Add(TopRFTLines[i]);
             }
 
-            //Case of Cantilever end Span
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[SpanVals.Length].SelectedRestraint != Restraints.NoRestraints)
-            {
-                TopRFTLines[TopRFTLines.Length - 1] = new GLine(gCanvas, new Point(StartPointTopArr[TopRFTLines.Length - 2].X + 0.80 * (EndPointTopArr[TopRFTLines.Length - 2].X - StartPointTopArr[TopRFTLines.Length - 2].X), StartPointTopArr[TopRFTLines.Length - 2].Y + 0.07 * scale),
+            TopRFTLines[TopRFTLines.Length - 1] = new GLine(gCanvas, new Point(StartPointTopArr[TopRFTLines.Length - 2].X + 0.80 * (EndPointTopArr[TopRFTLines.Length - 2].X - StartPointTopArr[TopRFTLines.Length - 2].X), StartPointTopArr[TopRFTLines.Length - 2].Y + 0.07 * scale),
                 new Point(EndPointTopArr[TopRFTLines.Length - 2].X, EndPointTopArr[TopRFTLines.Length - 2].Y + 0.07 * scale));
-                //add to the CANVAS 
-                GeometryEngineRFT.Shapes["RFT"].Add(TopRFTLines[TopRFTLines.Length - 1]);
-            }
-            
+            //add to the CANVAS 
+            GeometryEngineRFT.Shapes["RFT"].Add(TopRFTLines[TopRFTLines.Length - 1]);
         }
 
         #endregion
@@ -321,23 +221,7 @@ namespace Design.Presentation.ViewModels
         public static void LeftSecStirrups(GCanvas gCanvas, GeometryEngine GeometryEngineRFT, double scale)
         {
             double spacingLeft = 0;
-
-            //For Cantilever At Start:
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[0].SelectedRestraint != Restraints.NoRestraints)
-            {
-                spacingLeft = 0;
-                for (int j = 0; j < StirrupsLeftSec.GetLength(1); j++)
-                {
-                    StirrupsLeftSec[0, j] = new GLine(gCanvas, new Point(StartPointTopArr[0].X + 0.05 * scale + (spacingLeft / 1000) * scale, StartPointTopArr[0].Y + 0.07 * scale)
-                        , new Point(StartPointBotArr[0].X + 0.05 * scale + (spacingLeft / 1000) * scale, StartPointBotArr[0].Y/* - 0.07 * scale*/));
-                    spacingLeft += 150;
-
-                    //add to the CANVAS 
-                    GeometryEngineRFT.Shapes["RFT"].Add(StirrupsLeftSec[0, j]);
-                }
-            }
-
-            for (int i = 1; i < StirrupsLeftSec.GetLength(0); i++)
+            for (int i = 0; i < StirrupsLeftSec.GetLength(0); i++)
             {
                 spacingLeft = 0;
                 for (int j = 0; j < StirrupsLeftSec.GetLength(1); j++)
@@ -355,7 +239,7 @@ namespace Design.Presentation.ViewModels
         public static void RightSecStirrups(GCanvas gCanvas, GeometryEngine GeometryEngineRFT, double scale)
         {
             double spacingRight = 0;
-            for (int i = 0; i < StirrupsRightSec.GetLength(0) - 1; i++)
+            for (int i = 0; i < StirrupsRightSec.GetLength(0); i++)
             {
                 spacingRight = 0;
                 for (int j = 0; j < StirrupsRightSec.GetLength(1); j++)
@@ -366,21 +250,6 @@ namespace Design.Presentation.ViewModels
 
                     //add to the CANVAS 
                     GeometryEngineRFT.Shapes["RFT"].Add(StirrupsRightSec[i, j]);
-                }
-            }
-
-            //For cantilever at End
-            if (GeometryEditorVM.GeometryEditor.RestraintsCollection[SpanVals.Length].SelectedRestraint != Restraints.NoRestraints)
-            {
-                spacingRight = 0;
-                for (int j = 0; j < StirrupsRightSec.GetLength(1); j++)
-                {
-                    StirrupsRightSec[StirrupsRightSec.GetLength(0) - 1, j] = new GLine(gCanvas, new Point(EndPointTopArr[StirrupsRightSec.GetLength(0) - 1].X - (0.05 + spacingRight / 1000) * scale, EndPointTopArr[StirrupsRightSec.GetLength(0) - 1].Y + 0.07 * scale)
-                        , new Point(EndPointBotArr[StirrupsRightSec.GetLength(0) - 1].X - (0.05 + spacingRight / 1000) * scale, EndPointBotArr[StirrupsRightSec.GetLength(0) - 1].Y/* -  0.07 * scale*/));
-                    spacingRight += 150;
-
-                    //add to the CANVAS 
-                    GeometryEngineRFT.Shapes["RFT"].Add(StirrupsRightSec[StirrupsRightSec.GetLength(0) - 1, j]);
                 }
             }
         }
